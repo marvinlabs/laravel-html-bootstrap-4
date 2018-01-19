ac39e5e6903a69010bd7fe71af44714c41e9f402
Vincent Mimoun-Prat <vpratfr@yahoo.fr>
2018-01-19 10:21:51 +0100

    Update for latest Bootstrap 4 stable

--- src/Bootstrap/Traits/BuildsStylesAndScripts.php
+++ src/Bootstrap/Traits/BuildsStylesAndScripts.php
@@ -7,7 +7,7 @@ namespace MarvinLabs\Html\Bootstrap\Traits;
  */
 trait BuildsStylesAndScripts
 {
-    private static $BS4_CDN = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3';
+    private static $BS4_CDN = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0';
 
     /**
      * Link to the latest font-awesome CSS served from maxcdn.bootstrapcdn.com
@@ -22,7 +22,7 @@ trait BuildsStylesAndScripts
         $urls = collect([
             'jquery'      => 'https://code.jquery.com/jquery-3.2.1.min.js',
             'jquery.slim' => 'https://code.jquery.com/jquery-3.2.1.slim.min.js',
-            'popper'      => 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js',
+            'popper'      => 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
             'bs4'         => self::$BS4_CDN . '/js/bootstrap.min.js',
         ]);
 
